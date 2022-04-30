021430f24762530f6566e2e0c3b0f7aeb3a730b8
Emily Cabaniss <94324680+emilycabaniss@users.noreply.github.com>
2022-04-30 11:11:22 -0400

    Encased code within function.

--- Accepting User Input - Open File.py
+++ Accepting User Input - Open File.py
@@ -1,9 +1,13 @@
 # Accepting User Input - .txt file 
-
-fname = input('Please enter a valid file name: ')
-try: 
-	fhand = open(fname)
-except: 
-	print('File: ', fname, 'cannot be opened.')
-	exit()
+def accept_user_input(fname):
+	'''
+	Function to accept user input.
+	Incorporation of exception handling to prevent malfunction. 
+	'''
+	fname = input('Please enter a valid file name: ')
+	try: 
+		fhand = open(fname)
+	except: 
+		print('File: ', fname, 'cannot be opened.')
+		exit()
 
